1a9748a257fcbcfa7217350cd884460b834c450f
saurabh sharma <saurabhcoded@gmail.com>
2023-01-20 12:53:54 +0530

    final

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const Router = require("express").Router();
+Router.get("/", (req, res) => {
+  res.render("index");
+});
+Router.get("/about", (req, res) => {
+  res.render("about-us");
+});
+Router.get("/contact", (req, res) => {
+  res.render("contact", {
+    message: "Contact Me Here",
+  });
+});
+Router.get("/services", (req, res) => {
+  res.render("services", {});
+});
+Router.get("/services/:service", (req, res) => {
+  res.render("service-detail", {});
+});
+Router.get("/portfolios", (req, res) => {
+  res.render("portfolio", {});
+});
+Router.get("/portfolios/:portfolio", (req, res) => {
+  res.render("portfolio-detail", {});
+});
+Router.get("/blogs", (req, res) => {
+  res.render("blog", {});
+});
+Router.get("/blogs/:blog", (req, res) => {
+  res.render("blog-detail", {});
+});
+Router.get("/pricing", (req, res) => {
+  res.render("pricing", {});
+});
+
+module.exports = Router;
